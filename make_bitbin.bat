b7c2ab0fcc73ab1a21afa5e9d9fce6fa61f2a4ef
Wenstrand <douglas.wenstrand@jhuapl.edu>
2026-03-16 19:53:00 -0400

    one more 2024.2 reference

--- make_bitbin.bat
+++ make_bitbin.bat
@@ -1,5 +1,5 @@
 : this file assumes you have a bitfile in the impl_1 directory, and turns it into a bit.bin file
 : for fpgautil
-call c:\Xilinx\Vivado\2024.2\settings64.bat
+call C:\AMDDesignTools\2025.2\Vivado\settings64.bat
 call bootgen -image doug.bif -arch zynq -process_bitstream bin -w on
 
